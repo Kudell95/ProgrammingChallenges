0427d9929fbe44d395ef65c2844ff6c4845ffaf2
Matt <kudell95@hotmail.com>
2019-06-27 19:11:07 +1000

    temp converter

--- temp_converter.py
+++ temp_converter.py
@@ -1,6 +1,14 @@
-
-
-
 choice = input('type \'f\' to convert from farenheit to celsius or type \'c\' to convert celsius to farenheit\n')
+convertedTemp = 2
+temp = float(input('Enter a temp: '))
 
-temp = float(input('Enter a temp: ')
+if choice == 'f':
+    convertedTemp = (temp - 32) * 5/9
+    print(round(convertedTemp,2), "°c")
+elif choice == 'c':
+    convertedTemp = (temp * 9/5) + 32 
+    print(round(convertedTemp,2), "°f")
+else:
+    print("cannot convert\n")
+    
+
